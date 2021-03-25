8ced0fee6bae4f224558c54f3c68504d3c9e5ecf
Tommi Leikomaa <tommi.leikomaa@gmail.com>
2021-03-25 22:40:38 +0200

    send basic auth

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { render } from 'react-dom';
 
 import './index.scss';
+import axios from 'axios';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,6 +13,8 @@ render(
   </StrictMode>,
   document.getElementById('root')
 );
+
+axios.defaults.headers.common.Authorization = `Basic ${process.env.REACT_APP_BASIC_AUTH}`;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
