dc48d8417fddee22039689b5c4d7751a0c323601
Tommi Leikomaa <tommi.leikomaa@gmail.com>
2021-03-29 20:03:46 +0300

    Streaming options

--- frontend/src/Base/BasePage.tsx
+++ frontend/src/Base/BasePage.tsx
@@ -1,8 +1,12 @@
 import React, { PropsWithChildren, ReactElement } from 'react';
 
+import { useLocation } from '@reach/router';
+import classNames from 'classnames';
+
 import SponsorCGI from '../images/cgi_600px.webp';
 import SponsorEtteplan from '../images/Etteplan_logo_rgb_300.png';
 import KanaliigaLogo from '../images/kanaliiga-logo-250px.png';
+import Utils from '../utils';
 import VideoBackground from '../VideoBackground/VideoBackground';
 import './BasePage.scss';
 
@@ -15,9 +19,18 @@ export interface BasePageProps {
 const BasePage = (props: PropsWithChildren<BasePageProps>): ReactElement => {
   const { title, subtitles, showSponsors, children } = props;
 
+  const query = new URLSearchParams(useLocation().search);
+  const stream = query.has('stream');
+
   return (
-    <div className="container">
-      <VideoBackground videoSources={["/apex-video-background-download-worlds-edge-6.mp4"]} />
+    <div className={classNames('container', { stream })}>
+      {!stream && (
+        <VideoBackground
+          videoSources={[
+            `${Utils.videoUrl}/apex-video-background-download-worlds-edge-6.mp4`,
+          ]}
+        />
+      )}
       <div className="left-column">
         <div className="column-content">
           {title && (
@@ -26,7 +39,7 @@ const BasePage = (props: PropsWithChildren<BasePageProps>): ReactElement => {
               {subtitles && (
                 <div className="subtitle-info">
                   {subtitles.map((subtitleItem) => (
-                    <div>{subtitleItem}</div>
+                    <div key={subtitleItem}>{subtitleItem}</div>
                   ))}
                 </div>
               )}
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -6,4 +6,6 @@ export default class Utils {
       ? `:${process.env.REACT_APP_API_PORT}`
       : ':3001'
   }${process.env.REACT_APP_API_BASE || ''}`;
+
+  static videoUrl = process.env.REACT_APP_VIDEO_HOST || '';
 }
